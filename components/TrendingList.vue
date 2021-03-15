ba7d3a4486b6db09c75a421a88db56dc547f4888
miily8310s <rillakerika@gmail.com>
2021-03-16 07:50:40 +0900

    change style TrendingList

--- components/TrendingList.vue
+++ components/TrendingList.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="grid grid-cols-10">
-    <div v-for="(movie, index) in list" :key="index" class="mr-4 mt-5">
+    <div
+      v-for="(movie, index) in list"
+      :key="index"
+      class="mr-4 mt-5 relative overflow-hidden"
+    >
       <div>
         <img
           :src="
@@ -8,12 +12,26 @@
             movie.poster_path
           "
           alt="movie.title"
+          class="w-96"
         />
       </div>
-      <h3 v-if="movie.media_type === 'movie'">{{ movie.title }}</h3>
-      <h3 v-else>{{ movie.name }}</h3>
-      <h3 v-if="movie.media_type === 'movie'">{{ movie.release_date }}</h3>
-      <h3 v-else>{{ movie.first_air_date }}</h3>
+      <div
+        class="absolute top-0 right-0 m-3 py-2 px-3 bg-yellow-300 rounded-2xl font-bold"
+      >
+        {{ getAverage(movie.vote_average) }}
+      </div>
+      <div
+        class="absolute bottom-0 left-0 p-3 bg-gray-800 w-full movie-info opacity-80"
+      >
+        <h3>{{ movie.media_type === 'movie' ? movie.title : movie.name }}</h3>
+        <h3>
+          {{
+            movie.media_type === 'movie'
+              ? movie.release_date
+              : movie.first_air_date
+          }}
+        </h3>
+      </div>
     </div>
   </div>
 </template>
@@ -23,7 +41,18 @@ import Vue from 'vue'
 export default Vue.extend({
   name: 'TrendingList',
   props: {
-    list: Array,
+    list: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    getAverage(average: string) {
+      if (Number.isInteger(average)) {
+        return average + '.0'
+      }
+      return average
+    },
   },
 })
 </script>
